c56f3a234c16cd3fba4b070b53a88814f08eb86f
Shrest Sharma <shrestsharma18@gmail.com>
2026-04-19 18:45:03 +0530

    Config: Linked real Firebase credentials to the application

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB0NsauoPWl4lsmVSCcYR_QG1zqHkaOD1c",
+  authDomain: "flighttracker-63e05.firebaseapp.com",
+  projectId: "flighttracker-63e05",
+  storageBucket: "flighttracker-63e05.firebasestorage.app",
+  messagingSenderId: "922121022548",
+  appId: "1:922121022548:web:2274ab5ed5e7c60208d1c1"
 };
 
 // Initialize Firebase
